5fe12ff8e844ba730126f19e81a7f78f9ec14c7e
Alfonso <alfonso.sastre@protonmail.ch>
2017-10-10 18:12:20 +0200

    Update deploy.sh
    update docker name

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 
 mkdir -p logs 
-docker run -d -p 3838:3838 -v $PWD/app:/srv/shiny-server/  -v $PWD/logs/:/var/log/shiny-server/  lipideando
+docker run -d -p 3838:3838 -v $PWD/app:/srv/shiny-server/  -v $PWD/logs/:/var/log/shiny-server/ asastrebruno/lipideando
